2630714779f3d07544d7b9734e4eaca314d01777
Yosef Mets <maxmets@gmail.com>
2024-11-12 17:52:32 +0100

    Process ...

--- stores/appStore.js
+++ stores/appStore.js
@@ -60,9 +60,9 @@ export const useAppStore = defineStore('app', () => {
     return res.sort( ( a, b ) => sortsFunctions[activeSort.value]( a, b ) );
   });
   const itemsMinMax = computed( () => {
-    // return { min: 0, max: 1000 }
-    const a = displayedItems.value?.map( ({price}) => price).sort( ( a, b ) => a-b );
-    return { min: a[0], max: a[a.length-1] };
+    return { min: 0, max: 1000 }
+    // const a = displayedItems.value?.map( ({price}) => price).sort( ( a, b ) => a-b );
+    // return { min: a[0], max: a[a.length-1] };
   } );
 
   const country = computed( () => countries.value?.[user.value.country]);
