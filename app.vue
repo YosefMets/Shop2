4e7f8ab3ddb7f49796d25b09b3f134b214ac40e3
Yosef Mets <maxmets@gmail.com>
2024-12-15 12:59:33 +0100

    DB test

--- app.vue
+++ app.vue
@@ -1,8 +1,17 @@
 <script setup>
 const { currency, activeGroup } = storeToRefs( useAppStore() );
+
+// if (process.server) {
+  const qwe = hubDatabase()
+  console.log( qwe );
+
+  const req = qwe.prepare('SELECT * FROM Customers');
+  const res = req.all();
+// }
+
 </script>
 
-<template>
+<template>={{res}}=
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
