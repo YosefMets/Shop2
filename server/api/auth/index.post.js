4440dd8b89757e17aa715471262a36afdada341e
Yosef Mets <maxmets@gmail.com>
2024-12-24 19:29:19 +0100

    Set Session

--- server/api/auth/index.post.js
+++ server/api/auth/index.post.js
@@ -1,10 +1,34 @@
 
 export default defineEventHandler( async (event) => {
+  const db = hubDatabase();
   const body = await readBody(event);
-
   const res = { session: event.session, body }
 
-  setCookie( event,  'serverLogs',  JSON.stringify( res ));
+  const { email, pass, firstName } = body;
 
-  return res;
+  const getCustomerPrepare = db.prepare(
+    `SELECT * FROM Customers WHERE Email = ?1`
+  );
+
+  let customer = await getCustomerPrepare.bind( email ).first();
+
+  if ( !customer ) {
+    const putCustomerToCustomers = await db.prepare(
+      `INSERT INTO Customers ("Email", "Pass") VALUES ( ?1, ?2 )`
+    ).bind( email, pass ).run();
+
+    customer = await getCustomerPrepare.bind( email ).first();
+  }
+
+  if ( customer?.Pass === pass ) {
+    const putCustomerToSessionPrepare = db.prepare(
+      `UPDATE Sessions SET CustomerId = ?1 WHERE SessionId = ?2`
+    ).bind( customer.Id, event.session?.SessionId );
+
+    const res = await putCustomerToSessionPrepare.run();
+  }
+
+  setCookie( event,  'serverLogs',  JSON.stringify( { customer, firstName } ));
+
+  return { customer, firstName };
 })
