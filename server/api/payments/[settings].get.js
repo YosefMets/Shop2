63d6f65458d7be3a70182852a51a77472b74a91a
Yosef Mets <maxmets@gmail.com>
2025-01-19 14:29:14 +0100

    Test paymentApi

---
+++ server/api/payments/[settings].get.js
@@ -0,0 +1,27 @@
+export default defineEventHandler( async (event) => {
+  const { session } = event;
+  if ( !session.CustomerId ) throw createError({ statusCode: 401, statusMessage: 'Unauthorized' });
+
+  const db = hubDatabase();
+  const { settings } = getRouterParams(event);
+
+  if (settings !== "settings") throw createError({ statusCode: 400, statusMessage: `Rout "${settings}" is undefined` });
+  const {result} = db.prepare(`
+      SELECT 
+        pmt.Id AS MethodTypeId,
+        pmt.Name AS MethodTypeName,
+        pp.Id AS ProviderId,
+        pp.Name AS ProviderName
+      FROM 
+        AllowedPaymentMethods apm
+      JOIN 
+        PaymentMethodTypes pmt ON apm.PaymentMethodTypeId = pmt.Id
+      JOIN 
+        PaymentProviders pp ON apm.PaymentProviderId = pp.Id
+      ORDER BY 
+        pmt.Name, 
+        pp.Name;
+    `).all();
+
+  return result;
+})
