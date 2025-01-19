90204c4a083f49dc46552bd729e5e97a30bb490c
Yosef Mets <maxmets@gmail.com>
2025-01-19 12:22:38 +0100

    Test cart

--- server/api/shippings/[shippingId].delete.js
+++ server/api/shippings/[shippingId].delete.js
@@ -13,7 +13,9 @@ export default defineEventHandler( async (event) => {
   const isDefault = db.prepare(`SELECT * FROM Shippings WHERE c = 1 AND CustomerId = ?;`)
     .bind(session.CustomerId).first();
 
-  if ( !!isDefault ) throw createError({ statusCode: 403, statusMessage: 'Forbidden' });
+  if ( !isDefault ){
+    throw createError({ statusCode: 403, statusMessage: 'Forbidden-' & JSON.stringify(isDefault) });
+  }
 
   const newDefault = await db.prepare(`UPDATE Shippings
       SET IsDefault = 1
