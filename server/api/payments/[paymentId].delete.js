b9d1c9799684a8de25cdf14226d0b0b2f9ee3ee9
Yosef Mets <maxmets@gmail.com>
2025-01-20 14:01:48 +0100

    PaymentsApi Put

--- server/api/payments/[paymentId].delete.js
+++ server/api/payments/[paymentId].delete.js
@@ -3,20 +3,20 @@ export default defineEventHandler( async (event) => {
   if ( !session.CustomerId ) throw createError({ statusCode: 401, statusMessage: 'Unauthorized' });
 
   const db = hubDatabase();
-  const { shippingId } = getRouterParams(event);
+  const { paymentId } = getRouterParams(event);
 
-  /// TODO: Check last shipping left
-  const res = await db.prepare(`DELETE FROM Shippings WHERE Id = ? AND CustomerId = ?;`)
-    .bind(shippingId, session.CustomerId).run();
+  /// TODO: Check last PaymentMethods left
+  const res = await db.prepare(`DELETE FROM PaymentMethods WHERE Id = ? AND CustomerId = ?;`)
+    .bind(paymentId, session.CustomerId).run();
 
-  const isDefault = db.prepare(`SELECT * FROM Shippings WHERE IsDefault = 1 AND CustomerId = ?;`)
+  const isDefault = db.prepare(`SELECT * FROM PaymentMethods WHERE IsDefault = 1 AND CustomerId = ?;`)
     .bind(session.CustomerId).first();
 
   if ( !isDefault ){
     throw createError({ statusCode: 403, statusMessage: 'Forbidden-' & JSON.stringify(isDefault) });
   }
 
-  const newDefault = await db.prepare(`UPDATE Shippings
+  const newDefault = await db.prepare(`UPDATE PaymentMethods
       SET IsDefault = 1
       WHERE CustomerId = ?
       LIMIT 1;`).bind(session.CustomerId).run();
--- server/api/payments/index.get.js
+++ server/api/payments/index.get.js
@@ -3,7 +3,7 @@ export default defineEventHandler( async (event) => {
   if ( !session.CustomerId ) throw createError({ statusCode: 401, statusMessage: 'Unauthorized' });
 
   const db = hubDatabase();
-  const {results} = await db.prepare(`SELECT * FROM Shippings WHERE CustomerId = ?;`)
+  const {results} = await db.prepare(`SELECT * FROM PaymentMethods WHERE CustomerId = ?;`)
     .bind(session.CustomerId).all();
 
 
