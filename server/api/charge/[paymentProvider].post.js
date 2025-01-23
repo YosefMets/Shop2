3b2df19c4b58d87443f16d2de49d9586c038ea3a
Yosef Mets <maxmets@gmail.com>
2025-01-23 17:58:16 +0100

    PaymentsApi Put

---
+++ server/api/charge/[paymentProvider].post.js
@@ -0,0 +1,40 @@
+import Stripe from 'stripe';
+
+const stripeCharge = async ({
+                              amount,
+                              currency,
+                              token: source,
+                              orderId: receipt_number
+  }) => {
+
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  const charge = await stripe.charges.create({
+    amount,
+    currency,
+    source,
+    receipt_number
+  });
+
+  return charge
+}
+
+export default defineEventHandler( async (event) => {
+
+  const { paymentProvider } = getRouterParams( event );
+  const body = await readBody( event );
+
+  let res;
+
+  switch ( paymentProvider ) {
+    case 'stripe':
+      res = await stripeCharge( body );
+      break;
+    default:
+      break;
+  }
+
+  return res;
+
+});
